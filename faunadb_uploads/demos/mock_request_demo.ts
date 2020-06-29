f21b9a2c0654ccec85a9f36cc131841fefb4c829
nikniknaks <nicholasalexanderadams@gmail.com>
2020-06-29 16:24:08 -0400

    Script to demo async iteration where the a chunk of a stream is waited for, and then a request is waited for. This demos how I can make batched uploads of chunks of data.

---
+++ faunadb_uploads/demos/mock_request_demo.ts
@@ -0,0 +1,24 @@
+
+const fs = require('fs')
+const Requests = require('../modules/mock_request.ts')
+const readStream = fs.createReadStream('../data/words.json', { encoding: "utf-8"} );
+
+// this script demos the use of async iteration to read chunks of a stream
+// while making mock requests (which represent uploads of chunks)
+
+const looper = async () => {
+  for (let request of Requests) {
+    const streamContent = await readStream.read()
+
+    // console.log('typeof streamContent: ', typeof streamContent)
+
+    console.log('streamContent.length: ', streamContent ? streamContent.length : streamContent)
+
+    // console.log('streamContent: ', streamContent)
+
+    const result = await request()
+    console.log('result: ', result)
+  }
+}
+
+looper()
