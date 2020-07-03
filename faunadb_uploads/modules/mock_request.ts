fe593fa2134b1bea919b833cb999c190a12a1f63
nikniknaks <nicholasalexanderadams@gmail.com>
2020-07-03 12:50:16 -0400

    Add some batching functionality to a mock request.

--- faunadb_uploads/modules/mock_request.ts
+++ faunadb_uploads/modules/mock_request.ts
@@ -1,16 +1,21 @@
 const pause = delaySeconds => new Promise(r => setTimeout(r, delaySeconds*1000))
 
-const mockRequestOne = async () => {
+const mockRequestOne = async (data) => {
   await pause(1)
   return 'one complete'
 }
 
-const mockRequestTwo = async () => {
+const mockRequestTwo = async (data) => {
   await pause(2)
   return 'two complete'
 }
 
-const mockRequestThree = async () => {
+const mockRequestThree = async (data) => {
+  let totalString = ''
+  for (const item of data) {
+    totalString += JSON.stringify(item)
+  }
+  console.log(Buffer.byteLength(Buffer.from(totalString)))
   await pause(3)
   return 'three complete'
 }
